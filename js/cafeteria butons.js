0af19f26dd298fb9cb541463538fec3acd1c9724
Santiago_Mendoza <santymendoza0106@gmail.com>
2025-04-11 22:57:56 -0500

    muchos cambios buenos

---
+++ js/cafeteria butons.js
@@ -0,0 +1,26 @@
+const nuevosProductos = document.querySelector(".menu")
+const leprank = document.querySelector(".prank")
+const regresoHome = document.querySelector(".regre")
+const subscripcion = document.querySelector(".subs")
+
+
+
+nuevosProductos.addEventListener("click", function(){
+    window.location.href = "../html/zonaenmantenimiento.html" 
+
+});
+
+leprank.addEventListener("click", function(){
+    window.location.href = "../html/index.html" 
+
+});
+
+regresoHome.addEventListener("click", function(){
+    window.location.href = "../html/Bitacora.html"
+
+});
+
+subscripcion.addEventListener("click", function(){
+    window.location.href = "../html/zonaenmantenimiento.html"  
+
+});
